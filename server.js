779285350dc3240d697c350eaef2dab8ac6a14a3
Abhay936 <sa5393359@gmail.com>
2026-04-23 12:14:04 +0530

    fix server

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`🚀 Server running on ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
